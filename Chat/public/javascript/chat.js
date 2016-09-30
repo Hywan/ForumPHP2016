58feea71e4896e2dc8efbb01688eac2f309cf287
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-09-30 07:57:54 +0200

    Add context menu actions.

--- Chat/public/javascript/chat.js
+++ Chat/public/javascript/chat.js
@@ -40,5 +40,21 @@ Controls.prototype.networkStatus = function (status) {
     }
 };
 
+function menu(item) {
+    switch (item) {
+        case 'documentation':
+            window.open('https://hoa-project.net/Literature/Hack/Websocket.html');
+
+            break;
+
+        case 'sources':
+            window.open('https://github.com/Hywan/ForumPHP2016');
+
+            break;
+
+        default:
+            console.error('Menu option “' + item + '” is unknown.');
+    }
+};
 
 new Chat('ws://127.0.0.1:8080');
