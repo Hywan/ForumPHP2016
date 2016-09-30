1ed6554ed88dfa4b8c45e076e395f8675b30f783
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-09-30 16:08:09 +0200

    Add support for a basic chat.

---
+++ Chat/ChatServer.php
@@ -0,0 +1,128 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Hoa\Consistency;
+use Hoa\Event;
+use Hoa\Socket;
+use Hoa\Websocket;
+
+class Person extends Websocket\Node implements JsonSerializable
+{
+    protected $_pseudo   = null;
+    protected $_personId = null;
+
+    public function setPersonId($id)
+    {
+        $old             = $this->_personId;
+        $this->_personId = $id;
+
+        return $old;
+    }
+
+    public function getPersonId()
+    {
+        return $this->_personId;
+    }
+
+    public function setPseudo($pseudo)
+    {
+        $old           = $this->_pseudo;
+        $this->_pseudo = $pseudo;
+
+        return $old;
+    }
+
+    public function getPseudo()
+    {
+        return $this->_pseudo;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id'     => $this->getPersonId(),
+            'pseudo' => $this->getPseudo()
+        ];
+    }
+}
+
+$server = new Websocket\Server(new Socket\Server('ws://127.0.0.1:8080'));
+$server->getConnection()->setNodeName(Person::class);
+
+$server->on(
+    'open',
+    function (Event\Bucket $bucket) {
+        echo 'New person', "\n";
+    }
+);
+
+$server->on(
+    'close',
+    function (Event\Bucket $bucket) {
+        echo 'Loss a person', "\n";
+
+        $source = $bucket->getSource();
+        $node   = $source->getConnection()->getCurrentNode();
+
+        $source->broadcast(
+            json_encode([
+                'type' => 'client/person/delete',
+                'id'   => $node->getPersonId()
+            ])
+        );
+    }
+);
+
+$server->on(
+    'message',
+    function (Event\Bucket $bucket) {
+        $data       = $bucket->getData();
+        $source     = $bucket->getSource();
+        $connection = $source->getConnection();
+        $node       = $connection->getCurrentNode();
+
+        if (false === $message = @json_decode($data['message'])) {
+            $bucket->getSource()->close();
+
+            return;
+        }
+
+        switch ($message->type) {
+            case 'server/person/new':
+                $node->setPersonId($message->id);
+                $node->setPseudo($message->pseudo);
+
+                $source->send(
+                    json_encode([
+                        'type'    => 'client/persons',
+                        'persons' => array_values($connection->getNodes())
+                    ])
+                );
+                $source->broadcast(
+                    json_encode([
+                        'type'   => 'client/person/new',
+                        'id'     => $node->getPersonId(),
+                        'pseudo' => $node->getPseudo()
+                    ])
+                );
+
+                break;
+
+            case 'server/message/new':
+                $bucket->getSource()->broadcast(
+                    json_encode([
+                        'type'     => 'client/message/new',
+                        'personId' => $node->getPersonId(),
+                        'message'  => $message->message
+                    ])
+                );
+
+                break;
+        }
+    }
+);
+
+echo 'Server is listening…', "\n";
+
+$server->run();
--- Chat/public/javascript/chat.js
+++ Chat/public/javascript/chat.js
@@ -8,22 +8,123 @@ function Chat(uri) {
     this.connection.onclose = function () {
         self.controls.networkStatus('offline');
     };
-    this.controls = new Controls();
+    this.connection.onmessage = function (event) {
+        var bucket = JSON.parse(event.data);
+        console.log(bucket);
+
+        if (!bucket.type) {
+            return;
+        }
+
+        switch (bucket.type) {
+            case 'client/person/new':
+                var person = new Person(bucket.id, bucket.pseudo);
+
+                self.persons[person.id] = person;
+                self.updatePersonCounter();
+
+                break;
+
+            case 'client/persons':
+                bucket.persons.forEach(
+                    function (_person) {
+                        var person = new Person(_person.id, _person.pseudo);
+
+                        self.persons[person.id] = person;
+                    }
+                );
+                self.updatePersonCounter();
+
+                break;
+            
+            case 'client/person/delete':
+                delete self.persons[bucket.id];
+                self.updatePersonCounter();
+
+                break;
+
+            case 'client/message/new':
+                self.newMessage(self.persons[bucket.personId], bucket.message);
+            
+                break;
+        }
+    };
+    this.controls      = new Controls();
+    this.persons       = {};
+    this.currentPerson = null;
+    this.thread        = document.getElementById('thread');
 
     var shootbox = document.getElementById('shootbox');
-    var message = document.getElementById('message');
+    var message  = document.getElementById('message');
 
     shootbox.addEventListener(
         'submit',
         function (event) {
             event.preventDefault();
 
-            console.log('> ' + message.value);
+            self.connection.send(
+                JSON.stringify({
+                    'type'   : 'server/message/new',
+                    'message': message.value
+                })
+            );
+            self.newMessage(self.currentPerson, message.value);
+
+            message.value = '';
 
             return false;
         },
         false
     );
+
+    window.setTimeout(
+        function () {
+            var person         = new Person(guid(), 'Hywan');
+            self.currentPerson = person;
+
+            self.connection.send(
+                JSON.stringify({
+                    'type'  : 'server/person/new',
+                    'id'    : person.id,
+                    'pseudo': person.pseudo
+                })
+            );
+        },
+        1000
+    );
+}
+
+Chat.prototype.newMessage = function (person, message) {
+    var messageElement       = document.createElement('li');
+    var messageHeaderElement = document.createElement('aside');
+    var messageAvatarElement = document.createElement('img');
+    var messagePseudoElement = document.createTextNode(person.pseudo);
+    var messageBodyElement   = document.createTextNode(message);
+
+    messageAvatarElement.setAttribute('src', '…');
+    messageAvatarElement.setAttribute('alt', '');
+    messageAvatarElement.setAttribute('role', 'presentation');
+
+    messageHeaderElement.appendChild(messageAvatarElement);
+    messageHeaderElement.appendChild(messagePseudoElement);
+
+    if (person.id === this.currentPerson.id) {
+        messageElement.setAttribute('data-message-type', 'me');
+    }
+
+    messageElement.appendChild(messageHeaderElement);
+    messageElement.appendChild(messageBodyElement);
+
+    this.thread.appendChild(messageElement);
+};
+
+Chat.prototype.updatePersonCounter = function () {
+    document.getElementById('personCounter').innerHTML = Object.keys(this.persons).length;
+};
+
+function Person(id, pseudo) {
+    this.id     = id;
+    this.pseudo = pseudo;
 }
 
 function Controls() {
@@ -58,3 +159,14 @@ function menu(item) {
 };
 
 new Chat('ws://127.0.0.1:8080');
+
+function guid () {
+    function s4 () {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+
+    return s4() + s4() + '-' + s4() + '-' +
+        s4() + '-' +
+        s4() + '-' +
+        s4() + s4() + s4();
+}
