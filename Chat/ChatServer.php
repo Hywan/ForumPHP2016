c1cda6e5134701f1d9410019dd27964a4d260145
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-10-18 11:26:55 +0200

    Prepare first lesson.

--- Chat/ChatServer.php
+++ Chat/ChatServer.php
@@ -92,55 +92,13 @@ $server->on(
             return;
         }
 
+        var_dump($message);
+        $bucket->getSource()->close();
+
+        return;
+
         switch ($message->type) {
-            case 'server/person/new':
-                $node->setPersonId($message->id);
-                $node->setPseudo($message->pseudo);
-
-                $source->send(
-                    json_encode([
-                        'type'    => 'client/persons',
-                        'persons' => array_values($connection->getNodes())
-                    ])
-                );
-                $source->broadcast(
-                    json_encode([
-                        'type'   => 'client/person/new',
-                        'id'     => $node->getPersonId(),
-                        'pseudo' => $node->getPseudo()
-                    ])
-                );
-
-                break;
-
-            case 'server/person/active':
-                $source->broadcast(
-                    json_encode([
-                        'type' => 'client/person/active',
-                        'id'   => $node->getPersonId()
-                    ])
-                );
-
-                break;
-
-            case 'server/person/inactive':
-                $source->broadcast(
-                    json_encode([
-                        'type' => 'client/person/inactive',
-                        'id'   => $node->getPersonId()
-                    ])
-                );
-
-                break;
-
-            case 'server/message/new':
-                $bucket->getSource()->broadcast(
-                    json_encode([
-                        'type'     => 'client/message/new',
-                        'personId' => $node->getPersonId(),
-                        'message'  => $message->message
-                    ])
-                );
+            case '…':
 
                 break;
         }
